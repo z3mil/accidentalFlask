3decdf44771f439ae4909d7bb188f067b62dd7f6
Yet Just Another Threat Actor <31926931+threatact0r@users.noreply.github.com>
2020-10-14 19:15:09 +1100

    accidentalFlask vulnerable server in Flask

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, json, request, jsonify
+import hashlib, random, time
+import arc4, binascii
+from colorama import Fore, Style
+
+api = Flask(__name__)
+
+def encryptMe(someString):
+    a = arc4.ARC4('This-is-an-encryption-key')
+    cipherTextBin = a.encrypt(someString)
+    return binascii.hexlify(cipherTextBin).decode()
+
+def randMe(x):
+    stringlets = "0987654321asdfghjkl"
+    blob = ''.join(random.choice(stringlets) for _ in range(x))
+    return blob
+
+@api.route('/token', methods=['POST'])
+def token():
+    rBody = request.json
+    # What we can use as a seed for token should never include the user credentials or identities
+    # rAddr = request.remote_addr
+    # rUserAgent = request.headers.get('User-Agent')
+    username = rBody['Username']
+    token = encryptMe(username)
+    res = {"Token":token}
+    res_json = json.dumps(res)
+    response = jsonify(res_json)
+    return response
+
+@api.route('/whoami', methods=['POST'])
+def whoami():
+    rBody = request.json
+    token = rBody['Token']
+    if token == adminToken:
+        res = {"Success":"You are logged in as admin"}
+    else:
+        res = {"Success":"You are logged in as an unprivileged user"}
+    return jsonify(json.dumps(res))
+
+if __name__ == '__main__':
+    adminToken = encryptMe("admin")
+    print("==> Generating \"admin\" token ... "+Fore.GREEN+adminToken+Style.RESET_ALL)
+    print("==> Exposing REST api at http://127.0.0.1:5000/token ...")
+    print("==> Exposing REST api at http://127.0.0.1:5000/whoami ...")
+    api.run()
