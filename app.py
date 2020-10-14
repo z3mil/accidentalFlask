98b0b656de983f302fe1b844c875be1b32786c7f
Yet Just Another Threat Actor <31926931+threatact0r@users.noreply.github.com>
2020-10-14 21:54:35 +1100

    images

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask, json, request, jsonify
-import arc4, binascii, random, sys
+import arc4, binascii, random, sys, time
 from colorama import Fore, Style
 
+# Remove Flask warning about production use
 cli = sys.modules['flask.cli']
 cli.show_server_banner = lambda *x: None
 
 api = Flask(__name__)
 
-def encryptMe(someString):
+# Create a token
+def tokeniseMeNow(text):
+    now = str(time.time())[:8]
+    nowToken = encryptMe(text+now)
+    return nowToken
+
+# Simple RC4 encryption, no nonce.
+def encryptMe(x):
     a = arc4.ARC4('This-is-an-encryption-key')
-    cipherTextBin = a.encrypt(someString)
+    cipherTextBin = a.encrypt(x)
     return binascii.hexlify(cipherTextBin).decode()
 
+# Not in use, use if can attack in high volumes e.g. no rate limits
 def randMe(x):
-    stringlets = "0987654321asdfghjkl"
+    stringlets = "0987654321asdfghjklzxcvbnmqwertyuiop"
     blob = ''.join(random.choice(stringlets) for _ in range(x))
     return blob
 
@@ -24,7 +33,7 @@ def token():
     # rAddr = request.remote_addr
     # rUserAgent = request.headers.get('User-Agent')
     username = rBody['Username']
-    token = encryptMe(username)
+    token = tokeniseMeNow(username)
     res = {"Token":token}
     res_json = json.dumps(res)
     response = jsonify(res_json)
@@ -41,7 +50,8 @@ def whoami():
     return jsonify(json.dumps(res))
 
 if __name__ == '__main__':
-    adminToken = encryptMe("admin")
+    # create an encrypted token for the "admin" string and echo to terminal
+    adminToken = tokeniseMeNow("admin")
     print("==> Generating \"admin\" token ... "+Fore.GREEN+adminToken+Style.RESET_ALL)
     print("==> Exposing REST api at http://127.0.0.1:5000/token ...")
     print("==> Exposing REST api at http://127.0.0.1:5000/whoami ...")
